aa8a139931661796e1611e6d0ef63bb79ebf6c69
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-15 18:55:49 +0800

    3-9: Setting Up Babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,13 @@
-console.log("App.js is running!")
+"use strict";
+
+console.log("App.js is running!");
 
 // JSX (Javascript XML)
-// var template = <p>This is JSX from App.js</p>;
-var template = React.createElement("h1", null, "Something new");
+var template = React.createElement(
+  "p",
+  null,
+  "Indecision App!"
+);
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot); 
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log("App.js is running!")
+
+// JSX (Javascript XML)
+var template = <p>Indecision App!</p>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot); 
