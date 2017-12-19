3e311822aae14873263669a42e319c76a7736bfe
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-19 22:44:30 +0800

    4-25: ES6 Classes: Part 2

--- src/playground/es6_classes_1.js
+++ src/playground/es6_classes_1.js
@@ -13,6 +13,51 @@ class Person {
   }
 }
 
+class Student extends Person {
+  constructor(name, age, major) {
+    // call it the parent class and passed down the default variable 
+    // from parent
+    super(name, age);
+    this.major = major;
+  }
+
+  hasMajor() {
+    return !!this.major;
+  }
+
+  getDescription() {
+    // this will get the result from parent's getDescription(), you can access it using super
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description +=  ` Their major is ${this.major}.`;
+    }
+
+    return description;
+  }
+}
+
+class Traveller extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  hasLocation() {
+    return !!this.homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if(this.hasLocation()) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`;
+    }
+
+    return greeting;
+  }
+}
+
 const me = new Person('Mikael Airlangga', 28);
 console.log(me);
 console.log(me.getGreeting());
@@ -22,3 +67,13 @@ const other = new Person();
 console.log(other);
 console.log(other.getGreeting());
 console.log(other.getDescription());
+
+const student = new Student('Airlangga Trianggoro', 19, 'Computer Science');
+console.log(student);
+console.log(student.hasMajor());
+console.log(student.getDescription());
+
+const traveller = new Traveller('Zelda Link', 20, 'Jakarta');
+console.log(traveller);
+console.log(traveller.hasLocation());
+console.log(traveller.getGreeting());
