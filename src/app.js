97dc5ed0bd822ead616a437fd3e3380b2085a317
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-19 23:07:52 +0800

    4-27: Nesting Components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
+  }
+}
 class Header extends React.Component {
   render() {
     return (
@@ -24,6 +36,17 @@ class Options extends React.Component {
     return (
       <div>
         Options component here
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option component here
       </div>
     );
   }
@@ -39,13 +62,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
