a64d85fbe13ae74e238c159d67d45520471b9733
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-25 00:44:10 +0800

    6-51: ES6 Import/Export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('app.js is running!');
+// import { square, add } from './utils';
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(2, 3));
+
+import { isAdult, canDrink } from './person';
+
+console.log(isAdult(17));
+console.log(canDrink(22));
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+console.log('utils.js is running');
+
+export const square = (x) => x * x;
+
+export const add = (a, b) => a + b;
+
+// or you can use this: 
+// export { square, add };
