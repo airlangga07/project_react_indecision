2a459c59ab0b24f1bfc18cef708de99139941d77
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-26 22:18:45 +0800

    7-60: Passing Children to Component

--- src/app.js
+++ src/app.js
@@ -2,4 +2,27 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+  return (
+    <div>
+      <p>Header</p>
+      {props.children}
+      <p>Footer</p>
+    </div>
+  )
+}
+
+const template = (
+  <div>
+    <h1>Page Title</h1>
+  </div>
+);
+
+ReactDOM.render((
+  <Layout>
+    <div>
+      <h1>Page Title</h1>
+      <p>This is my page</p>
+    </div>uuu
+  </Layout>
+), document.getElementById('app'));
