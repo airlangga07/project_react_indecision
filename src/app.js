87300d913fc0a33cb41a588d840ec3d61ab45139
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-27 23:18:36 +0800

    8-66: Reset that 0add .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
