0dcbf17de4c161633c115b3004217a8679b21131
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-28 22:36:58 +0800

    8-69: Styling the Option List

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -87,8 +87,10 @@ export default class IndecisionApp extends React.Component {
         <Header subTitle={subTitle} />
         <div className="container">
           <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick} />
+        <div className="widget">
           <Options options={this.state.options} handleDeleteOption={this.handleDeleteOption} handleDeleteOptions={this.handleDeleteOptions} />
           <AddOption handleAddOption={this.handleAddOption} />
+        </div>
         </div>
         <OptionModal selectedOption={this.state.selectedOption} handleModalDismiss={this.handleModalDismiss} />
       </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -4,9 +4,12 @@ import Option from './Option';
 const Options = props => {
   return (
     <div>
-      {props.options.length === 0 && <p>Please add an option to get started</p>}
-      <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.map(op => <Option key={op} handleDeleteOption={props.handleDeleteOption} option={op} />)}
+      <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>
+        <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
+      </div>
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started</p>}
+        {props.options.map(op => <Option key={op} handleDeleteOption={props.handleDeleteOption} option={op} />)}
     </div>
   )
 }
