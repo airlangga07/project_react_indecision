73d2a30c102ca1ca6af41d0dd179e7e9de241dd0
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-12-29 00:39:44 +0800

    8-71: Styling React-Modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -8,10 +8,12 @@ const OptionModal = (props) => {
       onRequestClose={() => props.handleModalDismiss()}
       contentLabel="Selected Option"
       ariaHideApp={false}
+      closeTimeoutMS={200}
+      className="modal"
     >
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
-      <button onClick={() => props.handleModalDismiss()}>Okay</button>
+      <h3 className="modal__title">Selected Option</h3>
+      {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+      <button className="button" onClick={() => props.handleModalDismiss()}>Okay</button>
     </ReactModal>
   ) 
 }
